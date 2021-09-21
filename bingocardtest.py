95bc500b9d01e438af23032376bafdf7d37a75f9
ccarter11 <64336584+ccarter11@users.noreply.github.com>
2021-09-21 12:17:05 -0400

    Add files via upload
    test file

---
+++ bingocardtest.py
@@ -0,0 +1,16 @@
+#bingo card program 
+
+
+#import bingo data
+from graphics import * 
+
+
+#create a bingo grid  (5x5 grid ), (x,y) overall dimension
+window = GraphWin("Card", 600, 800)
+
+#card= readcardtemplate
+
+#divide categories into a txt of adult categories and kid categories
+
+#populate card with random topic
+last = ""
