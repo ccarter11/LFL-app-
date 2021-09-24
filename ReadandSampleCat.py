20fe4529fa3348725061d8d269627fca48919262
Maddie Ford <44879148+maddiemay19@users.noreply.github.com>
2021-09-24 12:55:55 -0400

    txt reading and sampling
    Python code for reading the .txt category files and creating a randomized list of 24.

---
+++ ReadandSampleCat.py
@@ -0,0 +1,26 @@
+# READING TXT FILES
+
+from random import sample
+
+def main():
+
+    # Open adultCategories.txt file and read from it
+    # Splits each newline and omits "/n" from being in the list
+    with open('adultCategories.txt','r') as file:
+        adultCatList = file.read().splitlines()
+        print(adultCatList)
+
+    with open('kidCategories.txt','r') as file:
+        kidCatList = file.read().splitlines()
+        #print(kidCatList)
+
+    #Sample 24 random elements from list (does not repeat elements)
+        
+    adultCatRandom = sample(adultCatList,24)
+    print(adultCatRandom)
+
+    kidCatRandom = sample(kidCatList,24)
+    print(kidCatList)
+
+    # Now must plot strings from list into GUI
+    
